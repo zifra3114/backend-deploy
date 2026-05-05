ccbea9032806bcd36c90a8fbd360013911aa4f99
Zifra Firdous <164349484+zifra3114@users.noreply.github.com>
2026-05-06 00:01:09 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,14 +2,16 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-  name: {type: String , required: true} ,
-  email: {type: String , required: true , unique: true} ,
-  password: {type: String , required: true} 
-}, {timestamps : true})
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
 
+// password hash
 userSchema.pre('save', async function(next) {
- if (!this.isModified('password')) return next();
- this.password = await bcrypt.hash(this.password, 10);
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
-export default mongoose.model('User' , userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
